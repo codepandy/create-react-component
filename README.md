da5dd691fe78b3eda1f67909017c2bdb75e888ad
wangpengpeng <pengpeng.wang@ishansong.com>
2020-04-03 15:52:35 +0800

    init code

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # create-react-component
-一个创建react组件的脚手架命令行工具CLI
+
+一个创建 react 组件的脚手架命令行工具 CLI
+
+## 安装
+
+```bash
+npm install -g create-react-component
+```
+
+## 初始化项目
+
+```bash
+c-rc-c init <componentName>
+
+//比如
+c-rc-c init GroupButton
+```
+
+### 命令为什么是`c-rc-c`
+
+这是`create-react-component`的缩写。本来想写成`ccc`，感觉这个名字太随便了，哈哈。
+
+## 开发组件
+
+目录结构
+
+```bash
+.
+├── README.md           # 组件说明文件
+├── package-lock.json
+├── package.json
+├── src                 # 代码目录
+│   ├── assets          # 图片等静态资源
+│   ├── demo            # 测试组件目录
+│   ├── index.js        # 组件代码文件
+│   └── index.less
+├── webpack.base.js     # webpack共用配置
+├── webpack.dev.js      # webpack开发环境配置
+└── webpack.pro.js      # webpack生产环境配置
+```
+
+## 测试组件
+
+组件开发过程中，可以在 demo 中调用测试结果。
+
+```bash
+npm run start
+```
+
+## 发布组件到 npm 仓库中
+
+组件测试没问题后，就可以发布到 npm 仓库中，供他人和自己以后使用。
+
+```bash
+npm run pub
+```
+
+至于发布到 npm 仓库的相关配置，可以看这边文章：[《开发并发布和更新一个 npm 包》](https://blog.wangpengpeng.site/2020/01/08/%E5%BC%80%E5%8F%91%E5%B9%B6%E5%8F%91%E5%B8%83%E5%92%8C%E6%9B%B4%E6%96%B0%E4%B8%80%E4%B8%AAnpm%E5%8C%85/)
---
+++ index.js
@@ -0,0 +1,136 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const { program } = require("commander");
+const download = require("download-git-repo"); // 下载模板文件
+const chalk = require("chalk"); // 终端样字符串样式设置
+const symbols = require("log-symbols"); //各种日志级别的彩色符号
+const ejs = require("ejs"); //修改模版文件内容
+const ora = require("ora"); //优雅的终端转轮，就是loading，提示进行中
+const inquirer = require("inquirer"); //公共交互式命令行用户界面的集合。就是询问时输入
+const shell = require("shelljs");
+const isEnglish = /^[a-zA-Z]+\d*$/gi; //检查文件名是否是英文，只支持英文
+
+const package = require("./package.json");
+
+program
+  .version(package.version, "-v --version", "查看当前版本")
+  .command("init <name>")
+  .action(name => {
+    if (!isEnglish.test(name)) {
+      console.log(symbols.error, chalk.red("名称只能有字母和数字组成！"));
+      return;
+    }
+    if (!fs.existsSync(name)) {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "author",
+            message: "请输入作者名称",
+          },
+          {
+            name: "description",
+            message: "请输入组件描述",
+          },
+          {
+            name: "keywords",
+            message: "请输入关键字,多个之间用英文逗号连接",
+          },
+          {
+            name: "repository",
+            message: "请输入你代码将要放置的仓库地址",
+          },
+          {
+            type: "list",
+            name: "cssPreprocessor",
+            message: "请选择css预处理器",
+            choices: ["less", "sass", "none"],
+          },
+        ])
+        .then(ansers => {
+          console.log(chalk.green("选项内容是："), ansers);
+          console.log(symbols.success, chalk.green("开始创建...........，请稍后"));
+          const spinner = ora(chalk.green("正在下载模板..."));
+          spinner.start();
+
+          download(
+            `direct:https://github.com/codepandy/react-component-template.git`,
+            name,
+            { clone: true },
+            err => {
+              if (err) {
+                spinner.fail(chalk.red("模板下载失败，请重新下执行！"));
+                console.log(symbols.error, chalk.red(err));
+                return;
+              }
+              spinner.succeed(chalk.green("模板下载成功！"));
+              const { author, description, keywords, repository, cssPreprocessor } = ansers;
+              const packageSpinner = ora(chalk.green("开始配置 package.json 文件..."));
+              try {
+                packageSpinner.start();
+                const subRepository = repository
+                  ? repository.substring(0, repository.indexOf("."))
+                  : "";
+                const packagePath = path.resolve(process.cwd(), name, "package.json");
+                console.log(chalk.yellow(`package.json地址：${packagePath}`));
+                const packageString = fs.readFileSync(packagePath, "utf8");
+                let packageJson = JSON.parse(packageString);
+                packageJson = {
+                  ...packageJson,
+                  author,
+                  description,
+                  keywords: keywords ? keywords.split(",") : [],
+                  repository: {
+                    type: `git`,
+                    url: `git+${repository}`,
+                  },
+                  bugs: {
+                    url: `${subRepository}/issues`,
+                  },
+                  homepage: `${subRepository}#readme`,
+                };
+
+                fs.writeFileSync(packagePath, JSON.stringify(packageJson));
+                packageSpinner.succeed("package.json文件配置成功！");
+              } catch (error) {
+                console.log(symbols.error, error);
+                packageSpinner.fail(chalk.red("文件配置失败！"));
+              }
+
+              const webpackSpinner = ora(chalk.green("开始配置 webpack ..."));
+              try {
+                webpackSpinner.start();
+                const webpackTemplatePath = path.resolve(process.cwd(), name, "webpack.base.ejs");
+                console.log(chalk.yellow(`webpack template 地址：${webpackTemplatePath}`));
+                const webpackTemplate = fs.readFileSync(webpackTemplatePath, "utf8");
+                const webpackString = ejs.render(webpackTemplate, { cssPreprocessor });
+                const webpackFilePath = path.resolve(process.cwd(), name, "webpack.base.js");
+                fs.writeFileSync(webpackFilePath, webpackString);
+                webpackSpinner.succeed(chalk.green("webpack 文件配置成功！"));
+                fs.unlinkSync(webpackTemplatePath); // 删除模板文件
+              } catch (error) {
+                webpackSpinner.fail(chalk.red("webpack 文件配置失败"));
+                console.log(chalk.red(error));
+              }
+
+              //安装依赖
+              const installDependenciesSpinner = ora(chalk.green(`安装依赖中...`));
+              installDependenciesSpinner.start();
+              shell.exec(
+                `cd ${name}
+              npm i`,
+                error => {
+                  if (error) {
+                    installDependenciesSpinner.fail(chalk.red(`依赖安装失败：${error}`));
+                  }
+                  installDependenciesSpinner.succeed(chalk.green("依赖安装完成"));
+                },
+              );
+            },
+          );
+        });
+    }
+  });
+
+program.parse(process.argv);
